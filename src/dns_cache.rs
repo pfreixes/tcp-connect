3a691cc9207ab860b946b6167d4f75795a616972
vangheem <vangheem@gmail.com>
2024-07-08 20:55:56 -0400

    fix compiler

--- src/dns_cache.rs
+++ src/dns_cache.rs
@@ -5,13 +5,13 @@
 use rand::prelude::*;
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::net::{SocketAddr, ToSocketAddrs};
-use std::sync::{Condvar, Mutex, RwLock, Arc};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::time::Instant;
-use std::thread::spawn;
-use tokio::time::Duration;
 use std::io::{Error, ErrorKind};
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Condvar, Mutex, RwLock};
+use std::thread::spawn;
+use std::time::Instant;
+use tokio::time::Duration;
 
 struct DNSCacheValue {
     addrs: Vec<SocketAddr>,
@@ -37,7 +37,10 @@ pub struct DNSCache<K> {
     queues: RwLock<HashMap<K, ResolutionQueue>>,
 }
 
-impl<K> DNSCache<K> {
+impl<K> DNSCache<K>
+where
+    K: Eq + PartialEq + Hash + Clone + ToSocketAddrs + Send + Sync + 'static,
+{
     // TODO: has to be pub(crate)
     pub fn new(ttl: Duration, max_stale: Duration) -> Self {
         DNSCache {
@@ -48,7 +51,6 @@ impl<K> DNSCache<K> {
         }
     }
 
-    /*
     fn resolve_in_background(&self, hostname: &K) -> ()
     where
         K: Eq + PartialEq + Hash + Clone + ToSocketAddrs + Send + 'static,
@@ -58,36 +60,28 @@ impl<K> DNSCache<K> {
         spawn(move || {
             let result = hostname.to_socket_addrs();
 
-            let cache = cache
-                .read()
-                .unwrap();
-            /*
+            let cache = cache.read().unwrap();
 
-            let mut value = cache.get(&hostname)
-                .unwrap()
-                .write()
-                .unwrap();
+            let mut value = cache.get(&hostname).unwrap().write().unwrap();
 
             match result {
                 Ok(addrs) => {
-                    let value = value.unwrap();
+                    let value = value.as_mut().unwrap();
                     value.addrs = addrs.collect();
                     value.inserted = Instant::now();
                     value.stale_refreshing = AtomicBool::new(false);
-                },
+                }
                 Err(_) => {
                     // In case of an error for the backgound refresher we do not
                     // bubble up the error, either other stale events will trigger
                     // new attempts or when the TTL is expired then we will make
                     // sure that we bubble up the error.
-                    let value = value.unwrap();
+                    let value = value.as_mut().unwrap();
                     value.stale_refreshing = AtomicBool::new(false);
                 }
             }
-            */
         });
     }
-    */
 
     // TODO: has to be pub(crate)
     pub fn lookup<A>(&self, hostname: &K, rng: &mut A) -> Result<SocketAddr, std::io::Error>
@@ -107,8 +101,7 @@ impl<K> DNSCache<K> {
                         .get(rng.gen::<usize>() % value.addrs.len())
                         .unwrap();
                     return Ok(socket_addr);
-                }
-                else if value.inserted + self.ttl + self.max_stale > now {
+                } else if value.inserted + self.ttl + self.max_stale > now {
                     // TODO: investigate the right ordering
                     /*
                     if let Ok(true) = value.stale_refreshing.compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed) {
@@ -136,7 +129,7 @@ impl<K> DNSCache<K> {
         if !queues.contains_key(hostname) {
             println!("lookup creating key!");
             let queue = ResolutionQueue {
-                shared : Mutex::new(SharedResolutionQueue{
+                shared: Mutex::new(SharedResolutionQueue {
                     is_resolving: false,
                     error: None,
                 }),
@@ -146,14 +139,9 @@ impl<K> DNSCache<K> {
         }
         drop(queues);
 
-        let queues = self
-            .queues
-            .read()
-            .unwrap();
+        let queues = self.queues.read().unwrap();
 
-        let queue = queues.get(hostname)
-            .unwrap();
-
+        let queue = queues.get(hostname).unwrap();
 
         let mut shared = queue.shared.lock().unwrap();
         if !shared.is_resolving {
@@ -164,33 +152,23 @@ impl<K> DNSCache<K> {
             drop(queues);
             match hostname.to_socket_addrs() {
                 Ok(addrs) => {
-                    let queues = self
-                        .queues
-                        .read()
-                        .unwrap();
+                    let queues = self.queues.read().unwrap();
 
-                    let queue = queues.get(hostname)
-                        .unwrap();
+                    let queue = queues.get(hostname).unwrap();
 
                     let mut shared = queue.shared.lock().unwrap();
 
-                    let cache = self
-                        .cache
-                        .read()
-                        .unwrap();
+                    let cache = self.cache.read().unwrap();
 
-                    let mut value = cache.get(hostname)
-                        .unwrap()
-                        .write()
-                        .unwrap();
+                    let mut value = cache.get(hostname).unwrap().write().unwrap();
 
-                    let addrs : Vec<SocketAddr> = addrs.collect();
+                    let addrs: Vec<SocketAddr> = addrs.collect();
                     let socket_addr = *addrs.get(rng.gen::<usize>() % addrs.len()).unwrap();
 
                     *value = Some(DNSCacheValue {
                         addrs: addrs,
                         inserted: Instant::now(),
-                        stale_refreshing: AtomicBool::new(false)
+                        stale_refreshing: AtomicBool::new(false),
                     });
 
                     drop(value);
@@ -202,15 +180,14 @@ impl<K> DNSCache<K> {
                     drop(queue);
                     drop(queues);
 
-                    return Ok(socket_addr)
+                    return Ok(socket_addr);
                 }
                 Err(err) => {
                     // TODO: figure out how to bubble up the orignal error
                     return Err(Error::new(ErrorKind::Other, err.to_string()));
                 }
             };
-        }
-        else {
+        } else {
             while shared.is_resolving {
                 shared = queue.cvar.wait(shared).unwrap();
             }
@@ -220,18 +197,15 @@ impl<K> DNSCache<K> {
                 return Err(Error::new(ErrorKind::Other, err.to_string()));
             }
 
-            let cache = self
-                .cache
-                .read()
-                .unwrap();
+            let cache = self.cache.read().unwrap();
 
-            let value = cache.get(hostname)
-                .unwrap()
-                .read()
-                .unwrap();
+            let value = cache.get(hostname).unwrap().read().unwrap();
 
             if let Some(value) = value.as_ref() {
-                let socket_addr = *value.addrs.get(rng.gen::<usize>() % value.addrs.len()).unwrap();
+                let socket_addr = *value
+                    .addrs
+                    .get(rng.gen::<usize>() % value.addrs.len())
+                    .unwrap();
                 return Ok(socket_addr);
             } else {
                 panic!("Not implemented");
